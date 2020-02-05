cafb9721d5fbb05d4ef0d40cb0b45e8b5a3dedaa
mudassarsyed <52317628+mudassarsyed@users.noreply.github.com>
2020-02-05 22:46:07 +0530

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -7,25 +7,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class SingleTest extends BrowserStackJUnitTest {
-  private static JSONObject config;
-
-    @Parameter(value = 0)
-    public int taskID;
-
-    @Parameters
-    public static Iterable<? extends Object> data() throws Exception {
-        JSONParser parser = new JSONParser();
-        config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + System.getProperty("config")));
-        int envs = ((JSONArray)config.get("environments")).size();
-
-        List<Integer> taskIDs = new ArrayList<Integer>();
-        for(int i=0; i<envs; i++) {
-            taskIDs.add(i);
-        }
-
-        return taskIDs;
-    }
-
 
   @Test
   public void test() throws Exception {
